eb6e6124f1bb28da609f3b7a191784a80f9a05c2
Harriet Hall <harriet.hall@mt-mbp-ccwvdyx213.home>
2024-04-10 15:57:12 +0100

    add countdown functionality to about page

---
+++ static/countdown.js
@@ -0,0 +1,30 @@
+const COUNT_LIMIT = 1000000000;
+let countPassed = 0;
+let countLeft = COUNT_LIMIT;
+
+let timerInterval = null;
+
+
+window.onload = 
+
+
+ function startCount() {
+
+    countInterval = setInterval(() => {
+
+       
+        countPassed = countPassed += 1;
+        countLeft = COUNT_LIMIT - countPassed;
+
+
+        formattedCountLeft = '$ '+`${countLeft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}` + ' reward for his capture, mashed or alive.'
+        document.getElementById("base-timer-label").innerHTML = formattedCountLeft
+
+       
+
+    },1000)
+
+};
+
+let currentCount = startCount()
+    window.localStorage(countPassed) 
